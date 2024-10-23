48d6b5a387a311b12187ff62670e520252009cf9
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2024-10-23 14:18:15 +0900

    [add] テストコードを追加

--- test/game_test.go
+++ test/game_test.go
@@ -3,10 +3,7 @@ package test
 import (
 	"log"
 	"net/url"
-	"os"
-	"os/signal"
 	"strconv"
-	"sync"
 	"testing"
 	"time"
 
@@ -46,55 +43,75 @@ func teardownWebSocketConnection(c *websocket.Conn, done chan struct{}) {
 	}
 }
 
-func TestGame(t *testing.T) {
+func TestConnectServer(t *testing.T) {
 	go func() {
 		server := core.NewServer()
 		server.Run()
 	}()
-	time.Sleep(1 * time.Second)
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
 
 	u := url.URL{Scheme: "ws", Host: config.WEBSOCKET_HOST + ":" + strconv.Itoa(config.WEBSOCKET_PORT), Path: "/ws"}
 	log.Printf("connecting to %s", u.String())
 
-	var wg sync.WaitGroup
+	c, done := setupWebSocketConnection(u, t)
+	defer teardownWebSocketConnection(c, done)
 
-	for i := 0; i < config.AGENT_COUNT_PER_GAME; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			c, done := setupWebSocketConnection(u, t)
-			defer teardownWebSocketConnection(c, done)
-
-			ticker := time.NewTicker(time.Second)
-			defer ticker.Stop()
-
-			for {
-				select {
-				case <-done:
-					return
-				case t := <-ticker.C:
-					err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-					if err != nil {
-						log.Printf("write: %v", err)
-						return
-					}
-				case <-interrupt:
-					log.Println("interrupt")
-
-					err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-					if err != nil {
-						log.Printf("write close: %v", err)
-						return
-					}
-					teardownWebSocketConnection(c, done)
-					return
-				}
-			}
-		}()
+	select {
+	case <-done:
+		log.Println("Connection closed")
+	case <-time.After(5 * time.Second):
+		log.Println("Test completed: Connection was successful for 5 seconds")
 	}
-
-	wg.Wait()
 }
+
+// func TestGame(t *testing.T) {
+// 	go func() {
+// 		server := core.NewServer()
+// 		server.Run()
+// 	}()
+// 	time.Sleep(1 * time.Second)
+
+// 	interrupt := make(chan os.Signal, 1)
+// 	signal.Notify(interrupt, os.Interrupt)
+
+// 	u := url.URL{Scheme: "ws", Host: config.WEBSOCKET_HOST + ":" + strconv.Itoa(config.WEBSOCKET_PORT), Path: "/ws"}
+// 	log.Printf("connecting to %s", u.String())
+
+// 	var wg sync.WaitGroup
+
+// 	for i := 0; i < config.AGENT_COUNT_PER_GAME; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			defer wg.Done()
+// 			c, done := setupWebSocketConnection(u, t)
+// 			defer teardownWebSocketConnection(c, done)
+
+// 			ticker := time.NewTicker(time.Second)
+// 			defer ticker.Stop()
+
+// 			for {
+// 				select {
+// 				case <-done:
+// 					return
+// 				case t := <-ticker.C:
+// 					err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+// 					if err != nil {
+// 						log.Printf("write: %v", err)
+// 						return
+// 					}
+// 				case <-interrupt:
+// 					log.Println("interrupt")
+
+// 					err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+// 					if err != nil {
+// 						log.Printf("write close: %v", err)
+// 						return
+// 					}
+// 					teardownWebSocketConnection(c, done)
+// 					return
+// 				}
+// 			}
+// 		}()
+// 	}
+
+// 	wg.Wait()
+// }
