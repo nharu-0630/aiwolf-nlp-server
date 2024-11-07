402a8a62382cfffca02919b79e59cf51ca4c94f5
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2024-11-07 23:33:15 +0900

    [add] リリースのActionsを追加

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"os"
+
 	"github.com/nharu-0630/aiwolf-nlp-server/core"
 	"github.com/nharu-0630/aiwolf-nlp-server/model"
 )
 
 func main() {
-	config, err := model.LoadConfigFromFile("./config/default.yml")
+	configPath := "./config/default.yml"
+	if len(os.Args) > 1 {
+		configPath = os.Args[1]
+	}
+	config, err := model.LoadConfigFromFile(configPath)
 	if err != nil {
 		panic(err)
 	}
