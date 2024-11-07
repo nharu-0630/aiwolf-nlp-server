07e6dabc17cc6fdc04815ae7c4d946693f8c2168
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2024-11-07 11:54:04 +0900

    [add] 5人人狼に騎士を追加

--- model/role.go
+++ model/role.go
@@ -68,8 +68,8 @@ func Roles(num int) map[Role]int {
 			R_POSSESSED: 0,
 			R_SEER:      1,
 			R_BODYGUARD: 0,
-			R_VILLAGER:  3,
-			R_MEDIUM:    0,
+			R_VILLAGER:  2,
+			R_MEDIUM:    1,
 		}
 	}
 	return nil
