0933aade8ddcbd4f86800132b6c270d81be6ec5e
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2024-12-12 13:50:39 +0900

    [add] 接続中のチームを確認するためのエンドポイントを追加

--- service/api_service.go
+++ service/api_service.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"maps"
+	"slices"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kano-lab/aiwolf-nlp-server/model"
 )
@@ -20,48 +23,65 @@ func NewApiService(analysisService *AnalysisService, config model.Config) *ApiSe
 func (api *ApiService) RegisterRoutes(router *gin.Engine) {
 	router.GET("/api/games", api.handleGameIDs)
 	router.GET("/api/game", api.handleGameData)
+	router.GET("/api/teams", api.handleTeams)
 }
 
 func (api *ApiService) handleGameIDs(c *gin.Context) {
-	gameIDs := api.getGameIDs()
-
+	if len(api.analysisService.gamesData) == 0 {
+		c.JSON(200, gin.H{"games": []string{}})
+		return
+	}
 	c.JSON(200, gin.H{
-		"games": gameIDs,
+		"games": slices.Collect(maps.Keys(api.analysisService.gamesData)),
 	})
 }
 
 func (api *ApiService) handleGameData(c *gin.Context) {
-	gameID := c.Query("id")
-	if gameID == "" {
+	id := c.Query("id")
+	if id == "" {
 		c.JSON(400, gin.H{"error": "id is required"})
 		return
 	}
-
-	gameData, exists := api.analysisService.gamesData[gameID]
+	data, exists := api.analysisService.gamesData[id]
 	if !exists {
 		c.JSON(404, gin.H{"error": "game not found"})
 		return
 	}
-
-	if !api.publishRunningGame && !api.analysisService.endGameStatus[gameID] {
+	if !api.publishRunningGame && !api.analysisService.endGameStatus[id] {
 		c.JSON(403, gin.H{"error": "game is running"})
 		return
 	}
-
-	responseData := gin.H{
-		"game_id":  gameID,
-		"win_side": gameData.winSide,
-		"agents":   gameData.agents,
-		"entries":  gameData.entries,
+	resp := gin.H{
+		"game_id":  id,
+		"win_side": data.winSide,
+		"agents":   data.agents,
+		"entries":  data.entries,
 	}
-
-	c.JSON(200, responseData)
+	c.JSON(200, resp)
 }
 
-func (api *ApiService) getGameIDs() []string {
-	var gameIDs []string
-	for gameID := range api.analysisService.gamesData {
-		gameIDs = append(gameIDs, gameID)
+func (api *ApiService) handleTeams(c *gin.Context) {
+	c.JSON(200, "Not implemented")
+	return
+
+	resp := gin.H{}
+	for id, data := range api.analysisService.gamesData {
+		if api.analysisService.endGameStatus[id] {
+			continue
+		}
+		teams := []string{}
+		for _, agent := range data.agents {
+			if agentMap, ok := agent.(map[string]interface{}); ok {
+				team := agentMap["team"].(string)
+				teams = append(teams, team)
+			}
+		}
+		for _, team := range teams {
+			if _, exists := resp[team]; !exists {
+				resp[team] = []string{}
+			}
+			resp[team] = append(resp[team].([]string), id)
+		}
 	}
-	return gameIDs
+	c.JSON(200, resp)
 }
